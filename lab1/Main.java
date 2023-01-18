927a35d5b655c01eec99c1209fa739edaf0a56b6
TPG1st <elliotpiper2003@gmail.com>
2023-01-18 14:31:31 +0100

    start lab1

--- lab1/Main.java
+++ lab1/Main.java
@@ -1,54 +1,40 @@
 class Main {
 
 	public static void main(String[] args) {
-		Animal.setYear(2019); // Så här anropar man en klassmetod,
-		// dvs en statisk metod.
-		// Hur ska koden för setYear se ut?
+		// Sätt årtalet för djuren.
+        Animal.setYear(2016);
 
-		Animal kurre = new Cat("Kurre", 6);
-		Animal vilma = new Dog("Vilma", 3);
+        // Skapa några djur.
+        Animal kurre  = new Cat("Kurre", 6);
+        Animal vilma  = new Dog("Vilma", 3);
+        Animal bamse  = new Cat("Bamse", 12);
+        Animal smilla = new Dog("Smilla", 1);
 
-		kurre.introduceYourself();
-		vilma.introduceYourself();
+        // Skapa leksaker.
+        Toy ball = new Toy ("Boll");
+        Toy shoe  = new Toy ("Tuggsko");
+        Toy mouse  = new Toy ("Plastmus");
 
-		Animal.setYear(2013); // Nu blir är ett nytt år för alla djur.
+        // Skapa huset.
+        House house = new House();
 
-		kurre.introduceYourself();
-		vilma.introduceYourself();
-//	  Animal kurre = new Cat("Kurre", 2003);
-//      Animal vilma = new Dog("Vilma", 2004);
-//      
-//      kurre.getBirthyear();
-//      vilma.getBirthyear();
-//      
-//      kurre.introduceYourself();
-//      vilma.introduceYourself();
-//    Animal cat       = new Animal("Cat", "Kurre");
-//    Animal dog       = new Animal("Dog", "Vilma");
-//    Animal bird      = new Animal("Bird", "Pippi");
-//    Animal butterfly = new Animal("Butterfly", "Bella");
-//
-//    System.out.println("==== Djuren introducerar sig ====");
-//    cat.introduceYourself();
-//    dog.introduceYourself();
-//    bird.introduceYourself();
-//    butterfly.introduceYourself();
+        // Skapa relationer mellan objekten.
+        house.addAnimal(kurre);
+        house.addAnimal(vilma);
+        house.addAnimal(bamse);
+        house.addAnimal(smilla);
 
-//    Animal[] allAnimals = new Animal[4];
-//    allAnimals[0] = cat;
-//    allAnimals[1] = dog;
-//    allAnimals[2] = bird;
-//    allAnimals[3] = butterfly;
-//
-//    System.out.println("==== Djuren gör sina ljud ====");
-//    for (int i = 0; i < allAnimals.length ; i++) {
-//      System.out.println(makeSound(allAnimals[i]));
-//    }
-//      
-//    System.out.println("==== Djuren flyger (om de kan) ====");
-//    for (int i = 0; i < allAnimals.length ; i++) {
-//      fly(allAnimals[i]);
-//    }
+        kurre.setFriend(vilma);
+        vilma.setFriend(smilla);
+        bamse.setFriend(kurre);
+
+        kurre.addToy(ball);
+        kurre.addToy(mouse);
+        vilma.addToy(shoe);
+        vilma.addToy(ball);
+
+        // Skriv ut vad som finns i huset.
+        house.print();
 
 	}
 
