abbc36f9e1905dc28702fe345524ccad2b5b92d5
TPG1st <elliotpiper2003@gmail.com>
2023-01-19 11:00:25 +0100

    l

--- lab1/House.java
+++ lab1/House.java
@@ -2,20 +2,19 @@
 import java.util.ArrayList;
 
 class House {
-	
-	ArrayList<Animal> animalList = new ArrayList<Animal>();
-	
+
+	ArrayList<Animal> animalList = new ArrayList<>();
+
 	public void print() {
-	    System.out.println("Följande djur finns i huset:");
-	    
-	    for (Animal animal : animalList) {
-	        animal.print();
-	    }
+		System.out.println("Följande djur finns i huset:");
+
+		for (Animal animal : animalList) {
+			animal.print();
+		}
 	}
-	
+
 	public void addAnimal(Animal animal) {
 		animalList.add(animal);
 	}
-	
 
 }
